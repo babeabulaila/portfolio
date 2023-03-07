500f287361152bddf1216ca3e618da0d288c90ae
Babe Abulaila <abeabulaila@gmail.com>
2023-03-07 16:04:35 -0500

    made section component, added filler text

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
-import { Container, Box, Heading, Image } from '@chakra-ui/react'
+import { Container, Box, Heading, Image, useColorModeValue } from '@chakra-ui/react'
+import Section from '@/components/section'
+import Paragraph from '@/components/paragraph'
+
 export default function Home() {
   return (
     <Container>
-      <Box borderRadius='lg' bg='red' p={3} align='center' margin={5}>
+      <Box borderRadius='lg' bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} align='center' margin={5}>
         Hello, I&apos;m a full-stack developer based in Brooklyn!
       </Box>
       <Box display={{ md: 'flex' }}>
@@ -27,9 +30,20 @@ export default function Home() {
             borderRadius='full'
             src='/images/square.png'
             alt='Profile Picture'
+            boxShadow='5px 5px 5px #505050'
           />
         </Box>
       </Box>
+      <Section delay={0.1}>
+        <Heading as='h3' variant='section-title'>
+          Work
+        </Heading>
+        <Paragraph>
+          Babe is a freelance and full-stack developer based in Brooklyn, New York with a passion for building digital solutions, and collaboration.
+          He has a knack for problem solving, music and technology. From planning and designing to debugging and launching, he respects the journey of each coding project.
+          When not in the codezone, he loves to write and record music in a plethora of genres. Currently, he is building more projects and learning Next.js to add to his repertoire.
+        </Paragraph>
+      </Section>
     </Container>
   )
 }
