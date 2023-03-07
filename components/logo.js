45976dc09599859ffe320729eac326fd61de0cee
Babe Abulaila <abeabulaila@gmail.com>
2023-03-06 21:49:02 -0500

    light and dark mode theme button added

--- components/logo.js
+++ components/logo.js
@@ -2,8 +2,6 @@ import Link from "next/link";
 import Image from "next/image";
 import { Text, useColorModeValue } from "@chakra-ui/react";
 import styled from "@emotion/styled";
-import darkCat from 'public/images/dark_cat.png'
-import lightCat from 'public/images/light_cat.png'
 
 const Logobox = styled.span`
 font-weight: bold;
@@ -18,12 +16,13 @@ padding: 10px;
     transform: rotate(-20deg);
 }
 `
+
 const Logo = () => {
-    const footPrintImg = `/public/images/dark_cat${useColorModeValue('', '-dark')}.png`
+    const catImg = `/images/cat${useColorModeValue('', '-dark')}.png`
     return (
         <Link href='/'>
             <Logobox>
-                <Image src={darkCat} width={30} height={30} alt='logo' />
+                <Image src={ catImg } width={30} height={30} alt='logo' />
                 <Text
                     color={useColorModeValue('gray.800', 'whiteAlpha.900')}
                     fontFamily='M PLUS rounded 1c'
